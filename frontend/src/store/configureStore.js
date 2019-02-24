34b2aaa841fc6aa927febbaa257b04215c3a331b
Hugo Freire <hugorfreire@gmail.com>
2019-02-24 11:48:58 -0300

    feat: change own logger for rexux-looger and using it only for development environment

--- frontend/src/store/configureStore.js
+++ frontend/src/store/configureStore.js
@@ -2,17 +2,22 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 import reducer from '../reducers'
 import effects from '../effects'
-
-import loggerMiddleware from '../middlewares/logger'
+import loggerMiddleware from 'redux-logger'
 import errorsMiddleware from '../middlewares/errors'
 
 export default function() {
   const sagaMiddleware = createSagaMiddleware()
 
+  const middlewares = [sagaMiddleware, errorsMiddleware];
+
+  if(process.env.NODE_ENV !== 'production') {
+    middlewares.push(loggerMiddleware);
+  }
+
   const store = createStore(
       reducer,
       compose(
-        applyMiddleware(sagaMiddleware, loggerMiddleware, errorsMiddleware)
+        applyMiddleware(...middlewares)
       )
   )
 
